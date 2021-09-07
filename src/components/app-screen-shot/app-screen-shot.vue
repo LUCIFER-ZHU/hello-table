f3f3629d728459ced4be96a1758a52ed6ed0afad
zhujiamin <57311383+LUCIFER-ZHU@users.noreply.github.com>
2021-09-07 10:29:35 +0800

    新增截图组件

---
+++ src/components/app-screen-shot/app-screen-shot.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="screen-shot">
+    <button @click="startshot">111111111111111111</button>
+    <ul>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+      <li>1112dwa dwa dwa  a dwad wa  dwad wa a da d a dw a dwa  dwa </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import axios from 'axios';
+import ScreenShort from "ibiz-web-screen-shot";
+@Component({})
+export default class appScreenShot extends Vue {
+
+    public a:any = null;
+
+    public startshot(){
+      this.a = new ScreenShort({
+        enableWebRtc :false,
+        completeCallback : this.completeCallback,
+        closeCallback : this.closeCallback,
+        level : 0,
+        canvasWidth :0,
+        canvasHeight :0
+      });  
+      console.log(this.a);
+      
+    }
+
+    public completeCallback(e:any){
+        console.log(e);
+         console.log(this.a);
+    }
+
+    public closeCallback(){
+      console.log('关闭了');
+      
+    }
+
+}
+</script>
+
+<style lang="less">
+@import './app-screen-shot.less';
+</style>
